312a9c13b40ebdb5075b87bc74079692e933905a
Dino Petrone <dino@madeinhaus.com>
2013-04-03 16:43:22 -0700

    clean up

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -5,8 +5,7 @@ from cloudseed.providers.ec2 import EC2Provider
 class TestEC2Provider(TestCase):
 
     def test_get_all_instances(self):
-        config = {'aws_key':'AKIAJADTWQVKNWOJUFIQ',
-                  'aws_secret':'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'}
+
         ec2 = EC2Provider(config)
         
 
