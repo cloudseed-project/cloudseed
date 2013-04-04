5797f1962398de1cd0281d898f7c413fd7b04a89
Adam Venturella <aventurella@gmail.com>
2013-04-04 08:40:22 -0700

    added logging

--- cloudseed/security.py
+++ cloudseed/security.py
@@ -1,7 +1,11 @@
 import os
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
 def add_key_for_config(key, config):
+    LOG.debug('Saving key for config')
     data = config.data
 
     filename = '{0}_{1}_{2}'.format(
@@ -9,10 +13,14 @@ def add_key_for_config(key, config):
         data['session'],
         data['provider'])
 
+    LOG.debug('Key filename: %s', filename)
+
     path = '{0}/.cloudseed/{1}/{2}'.format(
         os.path.expanduser('~'),
         data['project'],
         filename)
+
+    LOG.debug('Key path: %s', path)
 
     with open(path, 'w') as target:
         target.write(key)
