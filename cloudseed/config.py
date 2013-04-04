e5cc262ff1ccdd835407dd4f3c4fc9ae8e11e1ce
Adam Venturella <aventurella@gmail.com>
2013-04-03 19:00:16 -0700

    config update for session loading

--- cloudseed/config.py
+++ cloudseed/config.py
@@ -54,25 +54,41 @@ class DictConfig(object):
 
 class FilesystemConfig(object):
 
-    def __init__(self, local_config, project_config=None, global_config=None):
+    def __init__(self,
+        local_config,
+        project_config=None,
+        global_config=None,
+        session_config=None):
+
         self.data = self.config_for(
             local_config,
             project_config,
             global_config)
 
-        self.session = self.session_for(self.data)
+        if session_config:
+            self.session = self.session_for(session_config)
+        else:
+            try:
+                path = self.session_path(self.data)
+                self.session = self.session_for(path)
+            except RuntimeError:
+                self.session = {}
+                return
 
-    def session_for(self, config):
+    def session_path(self, config):
         try:
             session_id = config['session']
         except KeyError:
-            return {}
+            raise RuntimeError
 
         path = '{0}/{1}/session_{2}'.format(
             os.path.expanduser('~'),
             config['project'],
             session_id)
 
+        return path
+
+    def session_for(self, path):
         try:
             with open(path) as f:
                 return yaml.load(f)
