728572d9250563a49ca96f3058823e76c15dd52f
Adam Venturella <aventurella@gmail.com>
2013-04-03 18:24:37 -0700

    added skip ability

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -1,17 +1,21 @@
 import os
-from unittest import TestCase
+import unittest
 from mock import MagicMock
 from cloudseed.providers.ec2 import EC2Provider
 from cloudseed.config import Config
 from cloudseed.config import FilesystemConfig
 
+LIVE_EC2 = 'AWS_ACCESS_KEY_ID' in os.environ and \
+'AWS_SECRET_ACCESS_KEY' in os.environ
 
-class TestEC2Provider(TestCase):
+
+class TestEC2Provider(unittest.TestCase):
 
     def setUp(self):
         base_path = '{0}/tests/resources'.format(os.getcwd())
         self.config_ec2 = '{0}/config_ec2.yaml'.format(base_path)
 
+    @unittest.skipUnless(LIVE_EC2, 'AWS Environment not set')
     def test_get_all_instances(self):
         resource = FilesystemConfig(local_config=self.config_ec2)
         config = Config(resource, provider=MagicMock())
