7022babbd750827404260a60cb019133158635a7
Adam Venturella <aventurella@gmail.com>
2013-04-03 18:27:47 -0700

    tweaks

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -18,17 +18,10 @@ class TestEC2Provider(unittest.TestCase):
     @unittest.skipUnless(LIVE_EC2, 'AWS Environment not set')
     def test_get_all_instances(self):
         resource = FilesystemConfig(local_config=self.config_ec2)
-        resource.data['aws.key'] = 'asdf'
-        resource.data['aws.secret'] = 'asdf'
+        resource.data['aws.key'] = os.environ['AWS_ACCESS_KEY_ID']
+        resource.data['aws.secret'] = os.environ['AWS_SECRET_ACCESS_KEY']
 
         config = Config(resource, provider=MagicMock())
-
-        try:
-            config.data['aws.key'] = os.environ['AWS_ACCESS_KEY_ID']
-            config.data['aws.secret'] = os.environ['AWS_SECRET_ACCESS_KEY']
-        except:
-            #self.log.wargning('need to set both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env vars')
-            return
 
         ec2 = EC2Provider(config)
 
