953ab9500069f35ce103ae332b136e37544a7def
Adam Venturella <aventurella@gmail.com>
2013-05-02 08:19:32 -0700

    increased sleep time before running initial highstate

--- cloudseed/resources/Ubuntu-bootstrap.sh
+++ cloudseed/resources/Ubuntu-bootstrap.sh
@@ -26,4 +26,4 @@ mkdir -p /etc/salt/pki/minion
 mv master.pub /etc/salt/pki/minion/minion.pub
 mv master.pem /etc/salt/pki/minion/minion.pem
 apt-get install -y -o DPkg::Options::=--force-confold salt-minion
-sleep 1; /usr/bin/salt "master" state.highstate
+sleep 2; /usr/bin/salt "master" state.highstate
